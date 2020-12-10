db9019345b3e212d4021a5fe705287b8aca83ac9
Iris Lux <iris.lux0@gmail.com>
2020-12-09 22:17:38 -0800

    completed

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,27 @@
-const isPangram = function(text) {
 
+const isPangram = function(text) {
+  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+  const letters = text.toLowerCase().split('');
+  const letterCount = {};
+
+  letters.forEach(char => {
+    
+    if(letterCount[char]){
+      letterCount[char] += 1;
+    }
+    else{
+      letterCount[char] = 1;
+    }
+
+  })
+  console.log(letterCount);
+  for (const letter of alpha){
+    if(!letterCount[letter]){
+      console.log(letter);
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = isPangram;
